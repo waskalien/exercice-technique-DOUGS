9a68ffd25148fe4e48ff5bd5f58c8ff3b6c2874c
waskalien <lefebv.antoine@gmail.com>
2026-02-24 15:28:26 +0100

    refactor: remove unused default nestjs files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MovementsModule } from './movements/movements.module';
 
 @Module({
   imports: [MovementsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
